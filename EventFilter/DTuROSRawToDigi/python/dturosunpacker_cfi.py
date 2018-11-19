565c4b7cae552817960af5205667bfa2cddede17
Bilal Kiani <mkiani@users.noreply.github.com>
2018-11-19 16:32:16 +0100

    Delete dturosunpacker_cfi.py

--- EventFilter/DTuROSRawToDigi/python/dturosunpacker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-dturosunpacker = cms.EDProducer("DTuROSRawToDigi",
-                                  DTuROS_FED_Source = cms.InputTag("source"),
-                                  feds     = cms.untracked.vint32( 0,),
-                                  debug = cms.untracked.bool(True),
-                               )
