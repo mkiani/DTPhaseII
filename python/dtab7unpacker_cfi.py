87ad4ad9bd00e9b3433a37c376e0aa9c762dfeb8
Bilal Kiani <mkiani@users.noreply.github.com>
2018-12-12 15:43:15 +0100

    update

--- python/dtab7unpacker_cfi.py
+++ python/dtab7unpacker_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-dturosunpacker = cms.EDProducer("DTAB7RawToDigi",
+dtab7unpacker = cms.EDProducer("DTAB7RawToDigi",
                                   DTuROS_FED_Source = cms.InputTag("source"),
                                   feds     = cms.untracked.vint32( 1368,),
                                   debug = cms.untracked.bool(True),
