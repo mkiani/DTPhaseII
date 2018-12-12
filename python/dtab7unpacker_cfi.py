f9971757ef2dbeae39084d353fd86a978400813a
Bilal Kiani <mkiani@users.noreply.github.com>
2018-12-12 15:10:50 +0100

    Changing FED

--- python/dtab7unpacker_cfi.py
+++ python/dtab7unpacker_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 dturosunpacker = cms.EDProducer("DTAB7RawToDigi",
                                   DTuROS_FED_Source = cms.InputTag("source"),
-                                  feds     = cms.untracked.vint32( 0,),
+                                  feds     = cms.untracked.vint32( 1368,),
                                   debug = cms.untracked.bool(True),
                                )
