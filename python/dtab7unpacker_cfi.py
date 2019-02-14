cd8818f9befb13c0dea7581462f2e293d57d7f4a
muhammad Bilal Manzoor Kiani <mkiani@cern.ch>
2019-02-14 16:48:09 +0100

    Deleting

--- python/dtab7unpacker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-dtab7unpacker = cms.EDProducer("DTAB7RawToDigi",
-                                  DTAB7_FED_Source = cms.InputTag("source"),
-                                  feds     = cms.untracked.vint32( 1368,),
-                                  debug = cms.untracked.bool(True),
-                               )
